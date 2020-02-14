6a6b488e36043a2b27fbd81253de4b8ad76e3c5e
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-02-14 08:07:16 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+input.onButtonPressed(Button.B, function () {
+    forward(1000)
+})
+function forward (ms: number) {
+    basic.showIcon(IconNames.Yes)
+    basic.pause(1000)
+    basic.showArrow(ArrowNames.North)
+    pins.servoWritePin(AnalogPin.P1, 180)
+    pins.servoWritePin(AnalogPin.P2, 180)
+    basic.pause(2000)
+    pins.servoWritePin(AnalogPin.P1, 90)
+    pins.servoWritePin(AnalogPin.P2, 90)
+    basic.showIcon(IconNames.No)
+}
+input.onButtonPressed(Button.A, function () {
+    countdown = 5
+    for (let index = 0; index < 5; index++) {
+        basic.showNumber(countdown)
+        basic.pause(500)
+        countdown += -1
+    }
+    basic.showArrow(ArrowNames.North)
+    pins.servoWritePin(AnalogPin.P1, 180)
+    basic.pause(2000)
+    pins.servoWritePin(AnalogPin.P1, 90)
+    basic.showIcon(IconNames.No)
+})
+let countdown = 0
+basic.showIcon(IconNames.No)
